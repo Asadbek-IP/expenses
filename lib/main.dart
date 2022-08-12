d531f10f9f47cbe0993ff9c8530ed326357c6749
Asadbek <dasturchi6678@gmail.com>
2022-08-12 10:12:53 +0500

    first design

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ void main(List<String> args) {
 }
 
 class Expenses extends StatefulWidget {
-  const Expenses({ Key? key }) : super(key: key);
+  const Expenses({Key? key}) : super(key: key);
 
   @override
   State<Expenses> createState() => _ExpensesState();
@@ -16,7 +16,46 @@ class _ExpensesState extends State<Expenses> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(appBar: AppBar(title: Text('Harajatlar'),)),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Harajatlar'),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children:  [
+            Container(
+              width: double.infinity,
+            ),
+            Container(
+              child: const Card(
+                color: Colors.red,
+                elevation: 5,
+                child: Text(
+                  'Diagramma qismi',
+                  style: TextStyle(fontSize: 20),
+                ),
+              ),
+            ),
+            Container(
+              width: double.infinity,
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              child:   Card(
+                color: Colors.blue,
+                elevation: 5,
+                child: Container(
+                  margin: const EdgeInsets.all(10),
+                  child: const Text(
+                    'List Transtaction',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                ),
+              ),
+            ),
+            
+          ],
+        ),
+      ),
     );
   }
 }
