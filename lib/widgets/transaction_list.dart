7e0580405b8a37b55fe3a9d6e2e84d77d22ded19
Asadbek <dasturchi6678@gmail.com>
2022-08-25 10:00:00 +0500

    listview

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -10,69 +10,69 @@ class TrnsactionList extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       height: 380,
-      child: SingleChildScrollView(
-        child: Column(
-          children: transactions.map((tr) {
-            return Container(
-              padding: const EdgeInsets.symmetric(horizontal: 20),
-              child: Card(
-                elevation: 5,
-                child: Container(
-                  margin: const EdgeInsets.all(10),
-                  child: Row(
-                    children: [
-                      Container(
-                        padding: const EdgeInsets.all(6),
-                        decoration: BoxDecoration(
-                          border: Border.all(width: 2.0, color: Colors.purple),
-                        ),
-                        child: Row(
-                          crossAxisAlignment: CrossAxisAlignment.end,
-                          children: [
-                            Text(
-                              tr.price.toString(),
-                              style: const TextStyle(
-                                  fontSize: 20,
-                                  color: Colors.purple,
-                                  fontWeight: FontWeight.bold),
-                            ),
-                            Text(
-                              " so'm",
-                              style: TextStyle(
-                                  color: Colors.purple,
-                                  fontSize: 13,
-                                  fontWeight: FontWeight.w700),
-                            )
-                          ],
-                        ),
+      child: ListView.builder(
+        itemCount: transactions.length,
+          itemBuilder: ((context, index) => 
+           Container(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Card(
+              elevation: 5,
+              child: Container(
+                margin: const EdgeInsets.all(10),
+                child: Row(
+                  children: [
+                    Container(
+                      padding: const EdgeInsets.all(6),
+                      decoration: BoxDecoration(
+                        border: Border.all(width: 2.0, color: Colors.purple),
                       ),
-                      SizedBox(
-                        width: 20,
-                      ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.end,
                         children: [
                           Text(
-                            tr.title!,
-                            style: TextStyle(
-                                fontSize: 20, fontWeight: FontWeight.w700),
-                          ),
-                          SizedBox(
-                            height: 5,
+                            transactions[index].price.toString(),
+                            style: const TextStyle(
+                                fontSize: 20,
+                                color: Colors.purple,
+                                fontWeight: FontWeight.bold),
                           ),
                           Text(
-                            DateFormat.yMMMMd().format(tr.date!),
-                            style: const TextStyle(color: Colors.grey),
-                          ),
+                            " so'm",
+                            style: TextStyle(
+                                color: Colors.purple,
+                                fontSize: 13,
+                                fontWeight: FontWeight.w700),
+                          )
                         ],
                       ),
-                    ],
-                  ),
+                    ),
+                    SizedBox(
+                      width: 20,
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          transactions[index].title!,
+                          style: TextStyle(
+                              fontSize: 20, fontWeight: FontWeight.w700),
+                        ),
+                        SizedBox(
+                          height: 5,
+                        ),
+                        Text(
+                          DateFormat.yMMMMd().format(transactions[index].date!),
+                          style: const TextStyle(color: Colors.grey),
+                        ),
+                      ],
+                    ),
+                  ],
                 ),
               ),
-            );
-          }).toList(),
-        ),
+            ),
+          )
+          ),
+        
       ),
     );
   }
