3b51d2046169753f5cf8da9b5348bb41c9399397
asadbek-ip <dasturchi6678@gmail.com>
2022-09-12 19:36:32 +0500

    fonts

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:expenses/model/transaction.dart';
-import 'package:expenses/widgets/homePage.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
@@ -7,8 +6,26 @@ import 'package:intl/intl.dart';
 import 'widgets/new_transaction.dart';
 import 'widgets/transaction_list.dart';
 
-void main(List<String> args) {
-  runApp(MaterialApp(home: const Expenses()));
+void main(List<String> args) => runApp(MyApp());
+
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        fontFamily: "Quicksand",
+        appBarTheme: AppBarTheme(
+          color: Colors.purple
+        ),
+        accentColor: Colors.blue,
+        primarySwatch: Colors.purple
+      ),
+      home: Expenses(),
+    );
+  }
 }
 
 class Expenses extends StatefulWidget {
@@ -19,7 +36,7 @@ class Expenses extends StatefulWidget {
 }
 
 class _ExpensesState extends State<Expenses> {
-    List<Transaction> transactions = [
+  List<Transaction> transactions = [
     Transaction(
       id: "123",
       price: 60000,
@@ -39,54 +56,62 @@ class _ExpensesState extends State<Expenses> {
       date: DateTime.now(),
     ),
   ];
-   void _addTransaction(String name, double newprice) {
+  void _addTransaction(String name, double newprice) {
     setState(() {
-
-
-
       transactions.add(Transaction(
           id: "123", price: newprice, title: name, date: DateTime.now()));
     });
   }
 
-void startAddNewTr(BuildContext ctx){
-  showModalBottomSheet(backgroundColor: Colors.transparent,context: ctx, builder:(_)=>NewTransaction(addTr: _addTransaction));
-}
+  void startAddNewTr(BuildContext ctx) {
+    showModalBottomSheet(
+        backgroundColor: Colors.transparent,
+        context: ctx,
+        builder: (_) => NewTransaction(addTr: _addTransaction));
+  }
 
-
-
-  
   @override
   Widget build(BuildContext context) {
-    return 
-       Scaffold(
-        appBar: AppBar(
-          title: const Text('Harajatlar'),
-          actions: [
-            IconButton(onPressed:()=> startAddNewTr(context), icon: Icon(Icons.add,size: 32,))
-          ],
+    return Scaffold(
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: FloatingActionButton(
+        onPressed: ()=>startAddNewTr(context),
+        child: Icon(
+          Icons.add,
+          size: 36,
         ),
-        body: Column(
+      ),
+      appBar: AppBar(
+        title: const Text('Harajatlar'),
+        actions: [
+          IconButton(
+              onPressed: () => startAddNewTr(context),
+              icon: Icon(
+                Icons.add,
+                size: 32,
+              ))
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
           //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            
-              Container(
-                child: const Card(
-                  color: Colors.green,
-                  elevation: 5,
-                  child: Text(
-                    'Diagramma qis',
-                    style: TextStyle(fontSize: 20),
-                  ),
+            Container(
+              child: const Card(
+                color: Colors.green,
+                elevation: 5,
+                child: Text(
+                  'Diagramma qis',
+                  style: TextStyle(fontSize: 20),
                 ),
               ),
-            
+            ),
             TrnsactionList(transactions: transactions),
           ],
         ),
-      );
-    
+      ),
+    );
   }
 }
 // expenses
